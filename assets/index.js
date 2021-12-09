0b91af3ecb5547ef5e8b646d6faa68151bc55d39
Luis Mojica <Luis27mojica@gmail.com>
2021-12-09 12:18:04 -0500

    Agruegue mi primer plugin de mute-unmute

--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,10 @@
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
+const video = document.querySelector('video');
+const PlayButton = document.querySelector('#play-pause');
+const MuteButton = document.querySelector('#mute-unmute');
 
-function MediaPlayer(config){
-    this.media = config.el
-
-    MediaPlayer.prototype.play = () =>{
-            this.media.play();
-    };
-
-    MediaPlayer.prototype.pause = () => {
-        this.media.pause();
-    };
-};
-
-const video = document.querySelector("video");
-const button = document.querySelector("button");
-
-const player = new MediaPlayer({el: video});
-button.onclick = () => {player.media.paused ? player.play() : player.pause()};
+const player = new MediaPlayer({el: video, plugins: [new AutoPlay()]});
+PlayButton.onclick = () => player.togglePlay();
+MuteButton.onclick = () => player.sonido();
